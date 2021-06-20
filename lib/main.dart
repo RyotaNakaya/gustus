dbec073728d9d2d68e78fc6d060cff866e28a0e5
RyotaNakaya <0709nakaya@gmail.com>
2021-06-20 13:20:28 +0900

    ランクコンテンツページ追加

--- lib/main.dart
+++ lib/main.dart
@@ -70,17 +70,28 @@ class _RankListPageState extends State<RankListPage> {
                   // 取得した一覧を元にリスト表示
                   return ListView(
                     children: documents.map((document) {
-                      return Card(
-                        child: ListTile(
-                          title: Text(document['name']),
-                          trailing: IconButton(
-                            icon: const Icon(Icons.delete),
-                            onPressed: () async {
-                              await FirebaseFirestore.instance
-                                  .collection('ranks')
-                                  .doc(document.id)
-                                  .delete();
-                            },
+                      return InkWell(
+                        // ランクコンテンツページに遷移
+                        onTap: () {
+                          Navigator.of(context).pushReplacement(
+                            MaterialPageRoute(builder: (context) {
+                              return RankContentsPage(
+                                  document.id, document['name']);
+                            }),
+                          );
+                        },
+                        child: Card(
+                          child: ListTile(
+                            title: Text(document['name']),
+                            trailing: IconButton(
+                              icon: const Icon(Icons.delete),
+                              onPressed: () async {
+                                await FirebaseFirestore.instance
+                                    .collection('ranks')
+                                    .doc(document.id)
+                                    .delete();
+                              },
+                            ),
                           ),
                         ),
                       );
@@ -176,6 +187,83 @@ class _RankAddPageState extends State<RankAddPage> {
           ],
         ),
       ),
+    );
+  }
+}
+
+class RankContentsPage extends StatefulWidget {
+  final String rankId;
+  final String rankName;
+  // RankContentsPage({Key? rankId}) : super(key: rankId);
+  RankContentsPage(this.rankId, this.rankName);
+
+  @override
+  _RankContentsPageState createState() => _RankContentsPageState();
+}
+
+class _RankContentsPageState extends State<RankContentsPage> {
+  Widget build(BuildContext context) {
+    // final UserState userState = Provider.of<UserState>(context);
+    // final User user = userState.user!;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ランキングコンテンツ一覧'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Text(widget.rankId),
+          Text(widget.rankName),
+          Expanded(
+            child: StreamBuilder<QuerySnapshot>(
+              // TODO: 自分が登録したデータだけ fetch する
+              // TODO: rankId を元にランクコンテンツを取得する
+              stream: FirebaseFirestore.instance
+                  .collection('rank_contents')
+                  .orderBy('date')
+                  .snapshots(),
+              builder: (context, snapshot) {
+                // データが取得できた場合
+                if (snapshot.hasData) {
+                  final List<DocumentSnapshot> documents = snapshot.data!.docs;
+                  // 取得した一覧を元にリスト表示
+                  return ListView(
+                    children: documents.map((document) {
+                      return Card(
+                        child: ListTile(
+                          title: Text(document['name']),
+                          trailing: IconButton(
+                            icon: const Icon(Icons.delete),
+                            onPressed: () async {
+                              await FirebaseFirestore.instance
+                                  .collection('rank_contents')
+                                  .doc(document.id)
+                                  .delete();
+                            },
+                          ),
+                        ),
+                      );
+                    }).toList(),
+                  );
+                }
+                // データが読込中の場合
+                return const Center(
+                  child: Text('読込中...'),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () async {
+      //     await Navigator.of(context)
+      //         .push(MaterialPageRoute(builder: (context) {
+      //       return RankAddPage();
+      //     }));
+      //   },
+      //   child: const Icon(Icons.add),
+      // ),
     );
   }
 }
